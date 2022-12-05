f01e0f76c2fb088086671bcd5afe9a6115b0b52c
mkdemkov <mkdemkov@edu.hse.ru>
2022-12-05 22:14:57 +0300

    some methods aded to Book class

--- src/main/java/homework/Model/Book.java
+++ src/main/java/homework/Model/Book.java
@@ -3,12 +3,23 @@ package homework.Model;
 import java.util.ArrayList;
 
 public class Book {
+
+    private String title;
     private String description; // описание книги
     private ArrayList<String> authors; // авторы
     private int publishingYear; // год издания
 
     /**
-     * Получение описания книги
+     * Получить название книги
+     * @return название книги
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Получение описание книги
+     *
      * @return описание
      */
     public String getDescription() {
@@ -17,6 +28,7 @@ public class Book {
 
     /**
      * Получение списка авторов или выброс исключения, если авторов нет
+     *
      * @return список авторов
      */
     public ArrayList<String> getAuthors() throws BookException {
@@ -27,13 +39,22 @@ public class Book {
 
     /**
      * Получение года издания книги
+     *
      * @return год издания
      */
     public int getPublishingYear() {
         return publishingYear;
     }
 
-    public Book() {
+    public Book(String title, String description, ArrayList<String> authors, int publishingYear) {
+        this.title = title;
+        this.description = description;
+        this.authors = authors;
+        this.publishingYear = publishingYear;
+    }
 
+    public void printBook() {
+        // нужен фикс при выводе авторов
+        System.out.printf("\"%s\" была выпущена в %d году авторами - %s", title, publishingYear, authors.toString());
     }
 }
